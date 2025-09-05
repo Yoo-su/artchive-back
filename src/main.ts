750339d4b2e75276ef2f35293ee4dcbef9ac0b23
유수현 <rhan0871@naver.com>
2025-09-05 21:04:55 +0900

    feat: vercel 관련 설정

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,10 @@ import { AppModule } from './app/app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: process.env.CLIENT_DOMAIN,
+    origin: process.env.CLIENT_DOMAIN || 'http://localhost:3000',
     credentials: true, // 쿠키나 인증 헤더를 주고받기 위함
   });
-  await app.listen(process.env.PORT ?? 8000);
+  //await app.listen(process.env.PORT ?? 8000);
+  await app.init();
 }
 bootstrap();
