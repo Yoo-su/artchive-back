728ec65869f909937fd10459047feb4f49961c3c
유수현 <rhan0871@naver.com>
2025-09-06 10:57:11 +0900

    feat: railway 환경변수 path 설정

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,15 +12,11 @@ import { ChatModule } from '@/features/chat/chat.module';
 
 @Module({
   imports: [
-    // 1. 환경 변수 설정을 위한 ConfigModule
-    // isGlobal: true로 설정하여 모든 모듈에서 process.env 대신 ConfigService를 사용할 수 있게 합니다.
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: '.env', // 루트 디렉토리의 .env 파일을 사용
+      envFilePath: process.env.NODE_ENV === 'production' ? undefined : '.env',
     }),
 
-    // 2. 데이터베이스 연결을 위한 TypeOrmModule
-    // forRootAsync를 사용하여 ConfigService로부터 환경 변수를 주입받아 설정합니다.
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
@@ -32,7 +28,6 @@ import { ChatModule } from '@/features/chat/chat.module';
         autoLoadEntities: true,
       }),
     }),
-    // 3. 기능별 모듈 등록
     AuthModule,
     UserModule,
     BookModule,
@@ -43,8 +38,6 @@ import { ChatModule } from '@/features/chat/chat.module';
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
-    consumer
-      .apply(LoggerMiddleware) // LoggerMiddleware를 적용합니다.
-      .forRoutes('*'); // '*'는 모든 라우트에 적용하겠다는 의미입니다.
+    consumer.apply(LoggerMiddleware).forRoutes('*');
   }
 }
