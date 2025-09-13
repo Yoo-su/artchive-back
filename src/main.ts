cf8e7c8be7663736a06a2f1dc08b6f86814cd759
유수현 <rhan0871@naver.com>
2025-09-13 22:02:34 +0900

    feat: app 설정 추가

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,29 @@
 import { NestFactory } from '@nestjs/core';
+import { DataSource } from 'typeorm';
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: process.env.CLIENT_DOMAIN || 'http://localhost:3000',
-    credentials: true, // 쿠키나 인증 헤더를 주고받기 위함
+    credentials: true,
   });
+
+  app.enableShutdownHooks();
+
+  const dataSource = app.get(DataSource);
+
+  app
+    .getHttpAdapter()
+    .getInstance()
+    .on('close', async () => {
+      console.log('Server is closing, closing database connection...');
+      if (dataSource.isInitialized) {
+        await dataSource.destroy();
+        console.log('Database connection closed.');
+      }
+    });
+
   await app.listen(process.env.PORT ?? 8000);
 }
 bootstrap();
